002dfe3d666a7260df6dbfbe2a029a253fd6ada6
Henock-Lite <litehenock@gmail.com>
2025-01-10 01:39:08 +0100

    comment

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ document.addEventListener("drop", (e) => {
   if (e.target.getAttribute("data-draggable") == "target") {
     e.preventDefault();
     e.target.appendChild(item);
+     // e.target ici représente la zone où l'élément doit être déposé (à cause de l'événement "drop").
+    // Cela sélectionne l'élément cible (par exemple, une <ul> avec data-draggable="target").
+    // Ensuite, appendChild(item) ajoute l'élément glissé (item) en tant qu'enfant de la cible (par exemple, un <li> ajouté dans une <ul>).
   }
 });
 
